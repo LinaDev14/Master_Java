583656c1079720afb5d94cd071d1c69e1266a9cd
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-06-25 16:20:56 -0500

    se crea seccion 8

---
+++ src/main/java/co/com/linadev/seccion_8/JavaUntilDate.java
@@ -0,0 +1,10 @@
+package co.com.linadev.seccion_8;
+
+import java.util.Date;
+
+public class JavaUntilDate {
+    public static void main(String[] args) {
+
+        Date fecha = new Date();
+    }
+}
