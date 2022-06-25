b29a19c99c70e67b5df9298043293baeaf4f1d24
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-06-25 17:29:39 -0500

    se crea seccion 8

--- src/main/java/co/com/linadev/seccion_8/JavaUntilDate.java
+++ src/main/java/co/com/linadev/seccion_8/JavaUntilDate.java
@@ -1,10 +1,18 @@
 package co.com.linadev.seccion_8;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class JavaUntilDate {
     public static void main(String[] args) {
 
-        Date fecha = new Date();
+        Date date = new Date();
+
+        System.out.println("date = " + date);
+
+        // simple Date
+        SimpleDateFormat df = new SimpleDateFormat("dd/MMMM/yyyy");
+        String dateStr = df.format(date);
+        System.out.println("dateSrt = " + dateStr);
     }
 }
---
+++ src/main/java/co/com/linadev/seccion_8/JavaUtilCalendar.java
@@ -0,0 +1,33 @@
+package co.com.linadev.seccion_8;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class JavaUtilCalendar {
+    public static void main(String[] args) {
+
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+
+        //calendar.set(2022, Calendar.JUNE, 25);
+
+        calendar.set(Calendar.YEAR, 2020);
+        calendar.set(Calendar.MONTH, Calendar.JUNE);
+        calendar.set(Calendar.DAY_OF_MONTH, 25);
+
+        calendar.set(Calendar.HOUR_OF_DAY, 17);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 13);
+
+        
+        System.out.println("calendar = " + calendar);
+        System.out.println(date);
+        
+        // simple format
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateFormat = format.format(date);
+        System.out.println("dateFormat = " + dateFormat);
+
+    }
+}
